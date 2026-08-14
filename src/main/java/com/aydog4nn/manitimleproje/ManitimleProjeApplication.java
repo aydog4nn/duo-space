355bc5feddf494a86ed26814ae42b3ffbed5be1b
aydog4nn <aydog4nn@users.noreply.github.com>
2026-08-14 23:03:39 +0300

    first commit

---
+++ src/main/java/com/aydog4nn/manitimleproje/ManitimleProjeApplication.java
@@ -0,0 +1,13 @@
+package com.aydog4nn.manitimleproje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManitimleProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManitimleProjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aydog4nn/manitimleproje/ManitimleProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aydog4nn.manitimleproje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManitimleProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
